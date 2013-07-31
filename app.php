dbe951c30dd200b9407c30396fcb6fc57cc4aba3
Romain Schmitz <git@slopjong.de>
2013-08-01 01:45:10 +0200

    Adapted to the new Page class

--- app.php
+++ app.php
@@ -1,18 +1,7 @@
 <?php
 
-//********************************************************************
-// do not edit this section
-
 if(!defined("APPSDIR"))
     die("Direct access is not allowed!");
-
-$app_dir = realpath(dirname(__FILE__));
-// remove the full path of the document root
-$app_dir = str_replace(ROOTDIR, "", $app_dir);
-
-$page->setActivePage(basename($app_dir));
-
-//********************************************************************
 
 
 // inject your stylesheet if you need it
